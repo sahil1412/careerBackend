f9775f9ffbcfcda4d96caba08724c18523775f57
Sahil Kadian <sahilkadian4@gmail.com>
2024-10-19 15:21:24 +0530

    default otp and other

--- router/auth.js
+++ router/auth.js
@@ -40,6 +40,34 @@ router.get('/:Id',async (req,res)=>{
 
 const token = new Map();
 
+router.post('/addCompany',async (req,res)=>{
+    const {username, phone, companyName, email, employeeSize } = req.body;
+    console.log(req.body);
+    if(!username || !phone || !companyName || !email || !employeeSize){
+        return res.json({message : "Please fill all details"})
+    }
+
+    try{
+        const companyExist = await JobPost.findOne({phone : phone});
+        if(companyExist){
+            return res.json({message : "company already exists"});
+        };
+        const companyDetails = new User({username, phone, companyName, email, employeeSize });
+
+        const company = await companyDetails.save();
+        if(company){
+            res.json({message:"Job Posted successfully !!"});
+        }
+        else{
+            res.json({message : "Failed to Post"});
+        }
+
+    }
+    catch(err){
+        console.log(err);
+    }
+});
+
 router.post('/addPost',async (req,res)=>{
     const {jobTitle, jobDescription, experience, candidates, endDate} = req.body;
     console.log(req.body);
@@ -74,9 +102,8 @@ let otp, user;
 
 router.post("/signup",async(req,res) =>{
     try{
-        const {jobTitle, jobDescription, experienceLevel, candidates, endDate} = req.body;
-
-        const existingPost = await User.findOne({phone});
+        const {phone} = req.body;
+        const existingUser = await User.findOne({phone});
         if(existingUser){
             return res
             .status(400)
@@ -99,14 +126,16 @@ router.post("/signup",async(req,res) =>{
         }
         console.log(user);
         console.log(otp);
-        await client.messages
-        .create({
-            from : process.env.NUMBER,
-            to: `+91${phone}`,
-            body : `Your otp verification for ${username} is : ${otp}`
-        })
-        // .then(() => res.status(200).json({msg:"message sent"}))
-        .done();
+        // await client.messages
+        // .create({
+        //     from : process.env.NUMBER,
+        //     to: `+91${phone}`,
+        //     body : `Your otp verification for ${username} is : ${otp}`
+        // })
+         
+        return res
+        .status(400)
+        .json({"user" : user, "otp" : otp});
     } catch(e){
         return res.status(500).json({error:e.message});
     }
@@ -115,12 +144,19 @@ router.post("/signup",async(req,res) =>{
 router.post("/signup/verify",async(req,res)=>{
     try{
         console.log(req.body);
-        const{OTP} = req.body;
-        if(otp != OTP){
+        const {username, phone, companyName, email, employeeSize, phoneOTP } = req.body;
+        user = new User({
+            username,
+            phone,
+            companyName,
+            email,
+            employeeSize
+        })
+        
+        if(otp != phoneOTP && phoneOTP != "12345"){
             return res.status(400).json({msg : "Incorrect otp"});
         }
         user = await user.save();
-        // const token = jwt.sign({id:user._id},"passwordKey");
         res.status(200).json({msg : "user added successfully"});
         otp = "";
     } catch(e){
